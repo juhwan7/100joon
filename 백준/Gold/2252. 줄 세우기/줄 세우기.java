5ce815fdccec7daec000f9917f7c409f5b565933
juhwan7 <43717710+juhwan7@users.noreply.github.com>
2023-11-01 10:57:57 +0900

    [Gold III] Title: 줄 세우기, Time: 1792 ms, Memory: 181376 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            A.add(new ArrayList<>());
+        }
+        int indegree[] = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            int S = sc.nextInt();
+            int E = sc.nextInt();
+            A.get(S).add(E);
+            indegree[E]++; // 진입차수 배열 데이터 저장 부분
+        }
+        // 위상정렬 실행
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) { // 큐가 비어있을때까지
+            int now = queue.poll();
+            System.out.println(now + " ");
+            for (int next : A.get(now)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+
+                }
+            }
+        }
+    }
+}
