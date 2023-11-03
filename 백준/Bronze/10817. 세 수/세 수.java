72b802c868c9f2d6b5142866008eaf578331d882
juhwan7 <43717710+juhwan7@users.noreply.github.com>
2023-11-04 00:55:47 +0900

    [Bronze III] Title: 세 수, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] a = new int[3];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        System.out.println(a[1]);
+    }
+}
