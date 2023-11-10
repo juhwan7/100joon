715f8a937f43f66876ea2408c2b23f20097a023a
juhwan7 <43717710+juhwan7@users.noreply.github.com>
2023-11-10 15:15:46 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1436 ms, Memory: 201136 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                String[] str = st.nextToken().split("");
+                for (int j = str.length - 1; j >= 0; j--) {
+                    sb.append(str[j]);
+                }
+                sb.append(" ");
+            }
+            sb.append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
