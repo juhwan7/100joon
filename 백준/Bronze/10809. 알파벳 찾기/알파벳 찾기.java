f71bca4ff42a77b7588d2b50e46c317ecbdbd052
juhwan7 <43717710+juhwan7@users.noreply.github.com>
2023-11-13 18:23:22 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 148 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String S = br.readLine();
         int[] arr = new int[26];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = -1; // 기본 값 -1로 초기화
         }
+        String S = br.readLine();
         for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
             if (arr[ch - 'a'] == -1) { // 처음 들아온 값을 유지해야하기 때문에 값이 바뀌지 않은 -1인지 확인
